f9361529be071b73fa75da6260d19d91002d1daf
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-16 01:30:49 +0530

    Mongo Db Connect to JS

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+import { MongoClient } from "mongodb";
 
+async function main() {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+  //Executing the Query
+  const db = client.db("Vishal");
+  const messageColl = db.collection("message");
+  await messageColl.insertOne({ message: "Hi I am Vishal" });
+//Close the Connection
+  await client.close();
+  console.log("Record Added");
+}
+
+main();
