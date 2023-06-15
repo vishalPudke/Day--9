1e7050013abf8de1bc3913bf3b8500f98c81e49e
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-15 23:29:04 +0530

    9

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { readFileSync } from "node:fs";
-let filepath = "D:\Cdac\Cdac-2023-March\Module\WPT\WPT_MARCH";
-let fileData = readFileSync(filepath);
-console.log(fileData);
+import { readFileSync, writeFileSync } from "node:fs";
+let filepath = "D:CdacCdac-2023-MarchModuleWPTWPT_MARCH\vishal.txt";
+
+let data = "learn how to create a file Thank you Santosh sir and CDAC family";
+let fileData = writeFileSync(filepath, data);
+console.log("Write Success");
