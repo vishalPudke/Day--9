cd7f64e57f5a87a053cc35cd2e326033bb0e9f9c
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-16 00:25:20 +0530

    Async -read File

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { readFileSync } from "node:fs";
-let filepath = "D:\Cdac\Cdac-2023-March\Module\WPT\WPT_MARCH";
-let fileData = readFileSync(filepath);
-console.log(fileData);
+import { readFile } from "node:fs/promises";
+
+async function main() {
+  let filePath =
+    "D:CdacCdac-2023-MarchModuleWPTWPT_MARCHDay-7_Backend ProgrammingDay-9package.json";
+  let fileData = await readFile(filePath, { encoding: "utf-8" });
+  console.log(fileData);
+}
+main();
