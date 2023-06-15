c7e5fdb66a1ede25e0776379598f8530d153b011
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-16 01:04:46 +0530

    9

--- src/main.js
+++ src/main.js
@@ -1,15 +1,1 @@
-import { writeFile } from "node:fs/promises";
 
-async function main() {
-  try {
-    let filePath =
-      "D:Cdac/Cdac-2023-March/ModuleWPTWPT_MARCH/Day-7_Backend Programming/Day-9/vishal.txt";
-
-    let fileData = "Asynchronulsy sakhajksa sajhdkjs cusns vishal ;epraksahs ";
-    await writeFile(filePath, fileData);
-    console.log(fileData);
-  } catch (e) {
-    console.log("Exception Ocurred :: ", e.message);
-  }
-}
-main();
