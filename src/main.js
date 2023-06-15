f9d3d16d84ede7422840b9d14c6ec42f1dcf98ad
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-16 00:09:04 +0530

    using await we can get an output of non-blocikng asunchronous ::AYNCHRONOUS PROGRAMMING

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-console.log("Hello 1");
-// console.log("Hello 2");
-// setTimeout(() => console.log("Hello 2"),1000);
-setTimeout(() => {
-  console.log("Hello 2");
-}, 0);
-console.log("Hello 3");
-console.log("Hello 4");
+async function sum(n1,n2){
+    return n1+n2;
+}
+async function main(){
+    let value= await sum(140,3);
+    console.log(value);
+}
+main();
+
+
